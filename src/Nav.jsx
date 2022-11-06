2811afd296c34b9eed6c7a97500682b6aeafa4a0
master8848 <jobarsanjel2@gmail.com>
2022-11-06 21:41:39 +0545

    scroll bar changes

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,16 +3,14 @@ import Logo from "/cool care service logo.jpg";
 // import Call from 'C:\Users\sofni\Desktop\cooler_website_react\my-project\src\call.svg'
 export default function Nav() {
   return (
-
     <div className="m-0 p-0 w-screen h-full ">
       <img src={Logo} alt="" />
       <div className="flex justify-between bg-blue-900">
         <div>LOGO</div>
         <div className="my-auto font-serif">
           <h1 className="text-5xl font-bold`">Cool Care Service</h1>
-
         </div>
-        <div className="p-4 pr-10">
+        <div className="">
           <div className="font-bold text-lg">
             <ion-icon name="call-outline"></ion-icon> 9860732541
           </div>
@@ -21,13 +19,15 @@ export default function Nav() {
           </div>
         </div>
 
-
-        <div className='p-4 pr-10'>
-            <div className='font-bold text-lg'><ion-icon name="call-outline"></ion-icon> 9860732541</div>
-            <div className='font-bold text-lg'><ion-icon name="call-outline"></ion-icon> 9808589912</div>
+        <div className="">
+          <div className="font-bold text-lg">
+            <ion-icon name="call-outline"></ion-icon> 9860732541
+          </div>
+          <div className="font-bold text-lg">
+            <ion-icon name="call-outline"></ion-icon> 9808589912
+          </div>
         </div>
-    </div> 
-
+      </div>
 
       <div className="text-3xl cursor-pointer md:hidden">
         <ion-icon name="menu-outline"></ion-icon>
